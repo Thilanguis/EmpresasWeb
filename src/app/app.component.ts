99d37751bd9b8d58e034c0cf9245c79da9153f25
Gabriel Granado <gabrielt2005@hotmail.com>
2023-03-07 00:10:03 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'empresasweb';
+  
+  //Criando o modelo de dados do formulário
+  formCadastro = new FormGroup({
+
+    nomeFantasia: new FormControl('', [Validators.required]),
+    razaoSocial: new FormControl('', [Validators.required]),
+    cnpj: new FormControl('', Validators.required)
+    
+  });
+
+  //funcão para exibir as validações ndos campos na página
+  get form(): any {
+
+    return this.formCadastro.controls
+
+  }
+
+    //Função para processar o evento SUBMIT do formulário
+    onSubmit(): void{
+      console.log(this.formCadastro.value);
+    }
+
 }
